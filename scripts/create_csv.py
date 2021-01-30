7356740dbd46f996a2f3b71b6053f4e9e48560fa
Rafael R. Pertum <pertum@linse.ufsc.br>
2021-01-30 11:08:17 -0300

    created the  folder scripts and the script create_csv.py

---
+++ scripts/create_csv.py
@@ -0,0 +1,14 @@
+"""
+This script creates a new .CSV file with data information, the news (fake and true ones), the labels, the normalized texts and the preprocessed texts all togheter in the same dataframe. 
+
+Rafael R. Pertum - 31/01/2021
+
+@author:pertum    
+"""
+    
+import numpy as np
+import pandas as pd
+import os
+
+
+
