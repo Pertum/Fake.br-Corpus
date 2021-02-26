8bcce9e3bbdbcd1e0b237406648b2f0c2e9410b3
pertum <pertum@linse.ufsc.br>
2021-02-25 21:31:53 -0300

    adicionado novamente as noticias removidas

--- classification.py
+++ classification.py
@@ -14,12 +14,20 @@ sub_df = aux[['preprocessed', 'label']]
 
 # converte os diretórios para texto, aqui foi convertido os textos preprocessados em uma lista  
 preprocessed = func.dirs2texts(sub_df['preprocessed'])
+labels = sub_df['label'].to_numpy()
+
+#%%
+headers = ['preprocessed',
+            'label']
 
 # exemplo de um dataframe com os textos preprocessados e os labels
-sub_df_01 = pd.DataFrame([np.transpose(preprocessed), columns = func.preprocessed_content)
+sub_df_01 = pd.DataFrame(np.transpose([preprocessed, labels]), columns = headers)
 
 
-## extrair os metadados e criar um dataframe de exemplo com meta_content
 
-data_information = func.dirs2information(df['data_information'])
-sub_df_02 = pd.DataFrame(np.transpose([data_information]), columns=func.data_information_content)
+# ## extrair os metadados e criar um dataframe de exemplo com meta_content
+
+# data_information = func.dirs2information(df['data_information'])
+# sub_df_02 = pd.DataFrame(np.transpose([data_information]), columns=func.data_information_content)
+
+# %%
